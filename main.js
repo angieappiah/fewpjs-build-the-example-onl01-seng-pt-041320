1faef88469f854e2f774633935c3ee1a135e0832
angieappiah <alegnalawn2@yahoo.com>
2020-10-31 21:37:45 -0700

    commit tree 969bdd9ca9db6e90042080b2a7b06d37bc631181

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById('modal')
+const likeButtons = document.getElementsByClassName('like-glyph')
 
 
 
